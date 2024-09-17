477010fbf9ef8b5c20c8f70d85b1c1fe67edf1e6
taibislamdipu <mailtaibislam@gmail.com>
2024-09-17 13:31:48 +0600

    on progress

--- app/[lang]/page.js
+++ app/[lang]/page.js
@@ -3,12 +3,5 @@ import { getDictionary } from "./dictionaries";
 export default async function Home({ params: { lang } }) {
   const dictionary = await getDictionary(lang);
 
-  console.log("dictionary--->", dictionary);
-
-  return (
-    <div>
-      Welcome
-      {dictionary.hello}
-    </div>
-  );
+  return <div>{dictionary.hello}</div>;
 }
